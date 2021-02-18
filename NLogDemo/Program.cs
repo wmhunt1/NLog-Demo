45650591105d798009d09016f227a083958add53
Matt Hunt <wmatthunt@gmail.com>
2021-02-18 17:23:01 -0500

    works

---
+++ NLogDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NLog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger(); 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+             logger.Error("This is an error message");  
+                    Console.Read();  
+
+        }
+    }
+}
